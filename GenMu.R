d3abc05618166f69017ebe1943c2d864206048c6
lmerin91 <43382164+lmerin91@users.noreply.github.com>
2018-09-24 15:53:46 +0200

    Delete GenMu.R

--- GenMu.R
+++
@@ -1,8 +0,0 @@
-GenMu <- function(my, sig, n, Alph){
-  Theta=GenSuff(my, sig, n)
-  mu=Theta[1]+qt(Alph,
-                 df=n-1, 
-                 lower.tail = TRUE, 
-                 log.p = FALSE)*Theta[2]/sqrt(n)
-  return(mu)
-}
