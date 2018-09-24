b12b9d1163ae45335e329e0722c1af59ebfc410a
lmerin91 <43382164+lmerin91@users.noreply.github.com>
2018-09-24 14:50:42 +0200

    Update GenPalle.R

--- GenPalle.R
+++ GenPalle.R
@@ -3,14 +3,9 @@ GenPalle <- function(my, sig, n, m){
   p=rep(0,100)
   for (j in 1:100){
     teller=0
-    for (i in 1:m) {
-      Alph=0.01*j
-      mu[i]=GenMu(my, sig, n, Alph)
-      if (mu[i]>=my){
-        teller=teller+1
-      }
-      p[j]=teller/m
-      }
+    Alph=0.01*j
+    p_=GenP(my, sig, n, Alph, m)
+    p[j]=p_
     }
   return(p)
 }
